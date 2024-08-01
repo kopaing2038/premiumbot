598858c26168cf4aaa7a3379d54c50a2a7408a10
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-08-02 02:01:33 +0630

    Update channel.py

--- TechKP/plugins/channel.py
+++ TechKP/plugins/channel.py
@@ -1,8 +1,13 @@
 
 from ..config import Config
-from pyrogram import Client, filters
 from pyrogram.types import Message
-
+import asyncio
+import re
+from pyrogram import enums, filters, types
+from pyrogram.errors import (ChannelInvalid, UsernameInvalid,
+                             UsernameNotModified)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import types, enums, filters, Client
 from ..database import a_filter
 
 media_filter = filters.document | filters.video | filters.audio | filters.photo
@@ -19,10 +24,25 @@ async def media(bot: Client, message: Message):
         return
 
     media.file_type = file_type
-    if file_type == "photo":
-        media.file_name = message.caption
+    if message.media == enums.MessageMediaType.PHOTO:
+        media.file_type = "photo"
+        media.file_name = message.caption.split('\n')[0] if message.caption else ""
         media.mime_type = "image/jpg"
-    media.caption = message.caption
+    elif message.media == enums.MessageMediaType.VIDEO:
+        media.file_type = "video"
+        media.file_name = message.video.file_name if message.caption else ""
+        media.mime_type = "video/mp4"
+    elif message.media == enums.MessageMediaType.AUDIO:
+        media.file_type = "audio"
+        media.file_name = message.audio.file_name if message.caption else ""
+        media.mime_type = message.audio.mime_type if message.audio.mime_type else "audio/mpeg"
+    elif message.media == enums.MessageMediaType.DOCUMENT and message.document is not None:
+        media.file_type = "document"
+        media.file_name = message.document.file_name if message.document.file_name else ""
+        media.mime_type = message.document.mime_type if message.document.mime_type else ""
+                
+    media.caption = message.caption if message.caption else ""
     media.chat_id = message.chat.id
+    media.channel_name = message.chat.username or message.chat.title
     media.message_id = message.id
     await a_filter.save_file(media)
