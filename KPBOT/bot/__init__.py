eae1a31581294094f62214a1e1fdae6d2fb809d6
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-08-02 22:43:44 +0630

    Update __init__.py

--- KPBOT/bot/__init__.py
+++ KPBOT/bot/__init__.py
@@ -48,41 +48,33 @@ from pyrogram import Client, types
 from pyrogram.handlers import CallbackQueryHandler, InlineQueryHandler, ChosenInlineResultHandler, MessageHandler
 from TechKP.utils.logger import LOGGER
 
-async def resolve_listener(client: Client, update: types.Update):
+async def resolve_listener(
+    client: PatchedClient,
+    update: Union[types.CallbackQuery, types.Message, types.InlineQuery, types.ChosenInlineResult],
+):
     if isinstance(update, types.CallbackQuery):
-        await handle_callback_query(client, update)
-    elif isinstance(update, types.InlineQuery):
-        await handle_inline_query(client, update)
-    elif isinstance(update, types.ChosenInlineResult):
-        await handle_chosen_inline_result(client, update)
-    elif isinstance(update, types.Message):
-        await handle_message(client, update)
+        if update.message:
+            key = f"{update.message.chat.id}:{update.message.id}"
+        elif update.inline_message_id:
+            key = update.inline_message_id
+        else:
+            return
+    elif isinstance(update, (types.ChosenInlineResult, types.InlineQuery)):
+        key = str(update.from_user.id)
+    else:
+        key = str(update.chat.id)  # type: ignore
 
-async def handle_callback_query(client: Client, callback_query: types.CallbackQuery):
-    # Handle callback query here
-    LOGGER(__name__).info(f"Received callback query: {callback_query.data}")
-    await callback_query.answer("Callback query received!")
+    listener = client.listeners.get(key)
 
-async def handle_inline_query(client: Client, inline_query: types.InlineQuery):
-    # Handle inline query here
-    LOGGER(__name__).info(f"Received inline query: {inline_query.query}")
-    results = [types.InlineQueryResultArticle(
-        id="1",
-        title="Sample Result",
-        input_message_content=types.InputTextMessageContent(
-            message_text="This is a sample result"
-        )
-    )]
-    await inline_query.answer(results)
-
-async def handle_chosen_inline_result(client: Client, chosen_inline_result: types.ChosenInlineResult):
-    # Handle chosen inline result here
-    LOGGER(__name__).info(f"Chosen inline result: {chosen_inline_result.result_id}")
-
-async def handle_message(client: Client, message: types.Message):
-    # Handle messages here
-    LOGGER(__name__).info(f"Received message: {message.text}")
-    await message.reply("Message received!")
+    if listener and not listener["future"].done():  # type: ignore
+        if callable(listener["filters"]):
+            if not await listener["filters"](client, update):
+                update.continue_propagation()
+        listener["future"].set_result(update)  # type: ignore
+        update.stop_propagation()
+    else:
+        if listener and listener["future"].done():  # type: ignore
+            client.remove_listener(key, listener["future"])
 
 TechKPBot.add_handler(CallbackQueryHandler(resolve_listener), group=-2)
 TechKPBot.add_handler(InlineQueryHandler(resolve_listener), group=-2)
