54faa2bb5d07a74eb5e7b338993fdc0b99e6fe74
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-08-02 10:00:10 +0630

    Update and rename dm.pyg4g4g to dm.py

--- TechKP/plugins/helper/dm.py
+++ TechKP/plugins/helper/dm.py
@@ -5,10 +5,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters, enums
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
-async def evpm_tedvxt(bot, message):
+async def pm_text(bot, message):
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nBOT = @{bot.me.username}\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>")
-    await auto_filter(bot, message)
+    if Config.PM_SEARCH == True:
+        await auto_filter(bot, message)
+    else:
+        await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>")
