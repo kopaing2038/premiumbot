dbddaa38e10c5e8c2f190cb15a73ffd44ab94b99
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-08-02 09:58:07 +0630

    Update autofilter.py

--- TechKP/plugins/autofilter.py
+++ TechKP/plugins/autofilter.py
@@ -92,6 +92,16 @@ async def give_filter(client, message):
         except:
             pass
 
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+    if Config.PM_SEARCH == True:
+        await auto_filter(bot, message)
+    else:
+        await bot.send_message(chat_id=Config.LOG_CHANNEL, text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>")
 
 
 async def auto_filter(bot: Client, msg: types.Message, spoll=False , pm_mode = False):
