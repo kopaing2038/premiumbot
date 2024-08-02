5b2906b58d973ebae01d21fef15920450d90ffc1
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-08-03 00:45:38 +0630

    Update commands.py

--- TechKP/plugins/commands.py
+++ TechKP/plugins/commands.py
@@ -477,9 +477,9 @@ async def get_stats(_, msg: types.Message):
     msgs = await msg.reply("Fetching MongoDb DataBase")
     totalp = await a_filter.col.count_documents({})
     totalsec = await b_filter.col.count_documents({})
-    users = await db.get_uall_user()
-    chats = await db.get_all_chats()
-    premium_users = await db.get_all_premium()
+    users = await usersDB.total_users_count()
+    chats = await db.get_all_chats({})
+    premium_users = await db.get_all_premium({})
         
     stats = await db1.command('dbStats')
     used_dbSize = (stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))
