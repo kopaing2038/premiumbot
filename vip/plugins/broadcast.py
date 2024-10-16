668d97e601865052a562d4c93f552df28d2d65e0
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-10-17 01:04:10 +0630

    Update broadcast.py

--- vip/plugins/broadcast.py
+++ vip/plugins/broadcast.py
@@ -6,42 +6,33 @@ from vip.info import ADMINS
 from vip.utils import users_broadcast, get_readable_time, temp
 import asyncio
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup 
+from vip.utils import broadcast_messages, groups_broadcast_messages, temp, get_readable_time
+import asyncio
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 lock = asyncio.Lock()
-
-
-
 
 @Client.on_callback_query(filters.regex(r'^broadcast_cancel'))
 async def broadcast_cancel(bot, query):
     _, ident = query.data.split("#")
     if ident == 'users':
-        await query.message.edit("ᴛʀʏɪɴɢ ᴛᴏ ᴄᴀɴᴄᴇʟ ᴜsᴇʀs ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...")
+        await query.message.edit("Trying to cancel users broadcasting...")
         temp.USERS_CANCEL = True
-       
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_users(bot, message):
+               
+@Client.on_message(filters.command(["broadcast", "pin_broadcast"]) & filters.user(ADMINS) & filters.reply)
+async def users_broadcast(bot, message):
     if lock.locked():
         return await message.reply('Currently broadcast processing, Wait for complete.')
-
-    p = await message.reply('<b>Do you want pin this message in users?</b>', reply_markup=ReplyKeyboardMarkup([['Yes', 'No']], one_time_keyboard=True, resize_keyboard=True))
-    msg = await bot.listen(chat_id=message.chat.id, user_id=message.from_user.id)
-    if msg.text == 'Yes':
-        is_pin = True
-    elif msg.text == 'No':
-        is_pin = False
+    if message.command[0] == 'pin_broadcast':
+        pin = True
     else:
-        await p.delete()
-        return await message.reply_text('Wrong Response!')
-    await p.delete()
+        pin = False
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    b_sts = await message.reply_text(text='<b>ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs ᴛᴏ ᴜsᴇʀs ⌛️</b>')
+    b_sts = await message.reply_text(text='Broadcasting your users messages...')
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
-    blocked = 0
-    deleted = 0
     failed = 0
     success = 0
 
@@ -52,7 +43,7 @@ async def broadcast_users(bot, message):
                 temp.USERS_CANCEL = False
                 await b_sts.edit(f"Users broadcast Cancelled!\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
                 return
-            sts = await users_broadcast(int(user['id']), b_msg, is_pin)
+            sts = await broadcast_messages(int(user['id']), b_msg, pin)
             if sts == 'Success':
                 success += 1
             elif sts == 'Error':
@@ -60,7 +51,7 @@ async def broadcast_users(bot, message):
             done += 1
             if not done % 20:
                 btn = [[
-                    InlineKeyboardButton('CANCEL', callback_data=f'broadcast_cancel#users')
+                    InlineKeyboardButton('CANCEL', callback_data='broadcast_cancel#users')
                 ]]
                 await b_sts.edit(f"Users broadcast in progress...\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>", reply_markup=InlineKeyboardMarkup(btn))
         await b_sts.edit(f"Users broadcast completed.\nCompleted in {time_taken}\n\nTotal Users: <code>{total_users}</code>\nCompleted: <code>{done} / {total_users}</code>\nSuccess: <code>{success}</code>")
