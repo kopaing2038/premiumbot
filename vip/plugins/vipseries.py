8cefb377047169726b240700d4a8814d684bd30f
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-10-17 01:29:23 +0630

    Update vipseries.py

--- vip/plugins/vipseries.py
+++ vip/plugins/vipseries.py
@@ -3,36 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyb
 from pyrogram import errors, filters, types, Client
 from motor.motor_asyncio import AsyncIOMotorClient
 from vip.info import *
-
-
-client = AsyncIOMotorClient(DATABASE_URI)
-mydb = client["VIPREGISTERBOT"]
-
-class Database:
-    def __init__(self):
-        self.col = mydb.users
-
-
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
-        return bool(user)
-
-    async def add_user(self, id, name):
-        user = self.new_user(id, name)
-        await self.col.insert_one(user)
-
-    def new_user(self, id, name):
-        return dict(
-            id = id,
-            name = name,
-            point = 0,
-            ban_status=dict(
-                is_banned=False,
-                ban_reason=""
-            )
-        )
-db = Database()
-
+from vip.database.db import db
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def key_start(client, message):
