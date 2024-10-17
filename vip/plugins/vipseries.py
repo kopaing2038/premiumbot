0450ceeedaf50216a9b571c2f0ca24a0c7383b94
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-10-17 10:20:16 +0630

    Update vipseries.py

--- vip/plugins/vipseries.py
+++ vip/plugins/vipseries.py
@@ -17,25 +17,6 @@ async def stats(bot, message):
     
 
 
-from TechKP.plugins.autofilter import auto_filter
-from TechKP.config.config import Config
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto, ChatPermissions, WebAppInfo
-from pyrogram import Client, filters, enums, types
-
-
-@Client.on_message(filters.private & filters.text & filters.incoming)
-async def pm_search(bot, message):
-    content = message.text
-    user = message.from_user.first_name
-    user_id = message.from_user.id
-    mention = message.from_user.mention
-    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-
-    for admin in Config.ADMINS:
-        await bot.send_message(
-            chat_id=admin,
-            text=f"User Name : {mention} \nUser ID : <code>{user_id}</code>\nMᴇssᴀɢᴇ : <code>{content}</code>"
-        )
 
 
 @Client.on_message(filters.command("start") & filters.incoming)
