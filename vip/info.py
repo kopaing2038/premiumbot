6f1a46e55f093a3bac0fe39f55f746b44ed8ed9f
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-10-23 15:23:14 +0630

    Update info.py

--- vip/info.py
+++ vip/info.py
@@ -22,6 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001254905376'))
 START_IMG = os.environ.get('START_IMG', 'https://l.arzfun.com/30hBn')
 
+PAYMENT_QR = os.environ.get('PAYMENT_QR', 'https://graph.org/file/882df294b1ce4c2ddd02e.jpg')
 
 
 STATUS_TXT = """
@@ -31,3 +32,27 @@ NEW_USER_TXT = """<b>#New_User
 
 ≈ ɪᴅ:- <code>{}</code>
 ≈ ɴᴀᴍᴇ:- {}</b>"""
+
+
+PAYMENT_TEXT = """မင်္ဂလာပါ  
+
+1. English Series အတွက်က Lifetime ကိုမှ 4000 Kyats ပဲကျသင့်ပါမယ်။
+English Series List
+https://t.me/Serieslists
+
+2. Thailand Series အတွက်က Lifetime ကိုမှ 3000 Kyats ပဲကျသင့်ပါမယ်။
+Thailand Series List
+https://t.me/ThaiSeries_MTS
+
+3. Chinese Series အတွက်က Lifetime ကိုမှ 3000 Kyats ပဲကျသင့်ပါမယ်။ 
+Chinese Series List
+https://t.me/Chinese_Series_MCS
+
+4. Anime & Animation အတွက်က Lifetime ကိုမှ 3000 Kyats ပဲကျသင့်ပါမယ်။ 
+Anime & Animation List
+https://t.me/Anime_Animation_Series
+ 
+⭐️ ကြိုက်တဲ့ Series Channel 2 ခုကို Package Membership ဝင်ရင်တော့ Lifetime ကိုမှ 6000 Kyats ပဲ ကျသင့်ပါမယ်‌နော်။ (လူဦးရေကန့်သတ်ထားပါတယ်နော်။)
+
+⭐️ ကြိုက်နှစ်သက်ရာ Series Channel 3 ခုအား Package Membership Lifetime ကိုမှ 10000 Kyats နဲ့ မန်ဘာဝင်မယ်ဆိုရင် Series Channel 1 ခု အပိုဝင်ခွင်ရမှာပါနော်။ (လူဦးရေကန့်သတ်ထားပါတယ်နော်။)
+"""
