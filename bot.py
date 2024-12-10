1a838cfb65bd28e21f44b75628da724a6420f4e5
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-12-10 23:57:37 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,12 +54,12 @@ async def save_file(bot, file_name, file_id):
     }
     try:
         savecollection.insert_one(file)
-        print(f"Attempting to send file: {file_name}")
+        #print(f"Attempting to send file: {file_name}")
         
         # Validate file_id before sending
         try:
             await bot.send_video(chat_id=CHANNEL_ID, video=file_id, caption=file_name)
-            print(f"{file_name} is successfully saved.")
+            #print(f"{file_name} is successfully saved.")
             return True, 1
         except ValueError:
             print(f"Invalid file ID: {file_id}. Skipping.")
