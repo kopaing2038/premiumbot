1406f7a5763e4ff4726993210bee9fb4473b8062
kopaing2038 <126549210+kopaing2038@users.noreply.github.com>
2024-12-13 17:22:28 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def save_file(bot, file_name, file_id):
 
 
 async def send_videos(bot):
-    skip = 59600  # Number of files to skip
+    skip = 60000  # Number of files to skip
     current = skip  # Start current at -skip to adjust for skipped files
     
     videos = collection.find().skip(skip) 
